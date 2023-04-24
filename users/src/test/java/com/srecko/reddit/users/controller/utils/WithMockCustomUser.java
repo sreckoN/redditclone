ff7afdf05324170a53f7249a967d4db279d74079
sreckoN <srecko.nikolic@protonmail.com>
2023-04-24 22:14:51 +0200

    Added tests to Users microservice

---
+++ users/src/test/java/com/srecko/reddit/users/controller/utils/WithMockCustomUser.java
@@ -0,0 +1,14 @@
+package com.srecko.reddit.users.controller.utils;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import org.springframework.security.test.context.support.WithSecurityContext;
+
+@Retention(RetentionPolicy.RUNTIME)
+@WithSecurityContext(factory = com.srecko.reddit.controller.utils.WithMockCustomUserSecurityContextFactory.class)
+public @interface WithMockCustomUser {
+
+  String username() default "janedoe";
+
+  String[] authorities() default {};
+}
---
+++ users/src/test/java/com/srecko/reddit/users/service/utils/TestConfig.java
@@ -0,0 +1,16 @@
+package com.srecko.reddit.users.service.utils;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+  @Bean("testModelMapper")
+  public ModelMapper modelMapper() {
+    return new ModelMapper();
+  }
+}
